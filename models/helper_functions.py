5f890d40af0c068cc1ec12b58c5aefb33dcb3131
Giuseian <giuseppina.iannotti23@gmail.com>
2024-05-20 16:28:43 +0200

    added helper functions for models

---
+++ models/helper_functions.py
@@ -0,0 +1,42 @@
+# Helper Functions
+
+def get_pad_layer(pad_type):
+    if pad_type in ['reflect', 'refl']:
+        return nn.ReflectionPad2d
+    elif pad_type in ['replicate', 'repl']:
+        return nn.ReplicationPad2d
+    elif pad_type == 'zero':
+        return nn.ZeroPad2d
+    else:
+        raise NotImplementedError(f'Padding type {pad_type} not recognized')
+
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input * torch.rsqrt(torch.mean(input ** 2, dim=1, keepdim=True) + 1e-8)
+        
+def get_timestep_embedding(timesteps, embedding_dim, max_positions=10000):
+    assert len(timesteps.shape) == 1
+    half_dim = embedding_dim // 2
+    emb = math.log(max_positions) / (half_dim - 1)
+    emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=timesteps.device) * -emb)
+    emb = timesteps.float()[:, None] * emb[None, :]
+    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
+    if embedding_dim % 2 == 1:
+        emb = F.pad(emb, (0, 1), mode='constant')
+    return emb
+                                  
+class AdaptiveLayer(nn.Module):
+    def __init__(self, in_channel, style_dim):
+        super().__init__()
+        self.style = nn.Linear(style_dim, in_channel * 2)
+        self.style.bias.data[:in_channel] = 1
+        self.style.bias.data[in_channel:] = 0
+
+    def forward(self, input, style):
+        gamma, beta = self.style(style).chunk(2, 1)
+        gamma, beta = gamma.unsqueeze(2).unsqueeze(3), beta.unsqueeze(2).unsqueeze(3)
+        return gamma * input + beta
+
