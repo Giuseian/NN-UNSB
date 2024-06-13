66c1ee5f24cb7d69a80d4371265d108a9fad90b0
Giuseian <giuseppina.iannotti23@gmail.com>
2024-06-13 12:43:29 +0200

    added parser arguments

--- options/train_options.py
+++ options/train_options.py
@@ -1,7 +1,13 @@
 import argparse
 train_parser = argparse.ArgumentParser(description='UNSB Training')
 
-# Adding arguments to the parser
+# Adding arguments to the parserù
+train_parser.add_argument('--dataroot', default='./horse2zebra', help='path to images (should have subfolders trainA, trainB, valA, valB, etc)') 
+train_parser.add_argument('--path_trainA', type=str, required=True, default = 'new_trainA' , help='Path to training dataset A')
+train_parser.add_argument('--path_trainB', type=str, required=True, default = 'new_trainB', help='Path to training dataset B')
+train_parser.add_argument('--path_testA', type=str, required=True, deafult = 'test_A', help='Path to testing dataset A')
+train_parser.add_argument('--path_testB', type=str, required=True, default = 'test_B', help='Path to testing dataset B')
+train_parser.add_argument('--batch_size', type=int, default=1, help='batch size of train input data')
 train_parser.add_argument('--total_iters', type=int, default=0, help='Total number of iterations')
 train_parser.add_argument('--optimize_time', type=float, default=0.1, help='Time for optimization')
 train_parser.add_argument('--epoch_count', type=int, default=1, help='Starting count of epochs')
